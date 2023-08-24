8a6b5cbbdd40856f82e5c3a8c1dc8126f552714b
王洪亮 <w505957125@aliyun.com>
2023-08-24 08:52:54 +0800

    first commit

---
+++ spring-boot-oauth2-server/src/main/java/com/whl/spring/ServerBootstrap.java
@@ -0,0 +1,13 @@
+package com.whl.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBootstrap.class, args);
+    }
+
+}
